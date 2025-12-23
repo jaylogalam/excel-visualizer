dcc75a670d7afe3cca3887df7d0f2e1117f783f3
Jaylo <jaylogalam04@gmail.com>
2025-12-23 16:04:13 +0800

    components: create sub components for auth forms

---
+++ src/features/auth/components/src/index.ts
@@ -0,0 +1,7 @@
+import { Button } from "./button";
+import { InputText } from "./input-text";
+import { InputPassword } from "./input-password";
+import { FormError } from "./form-error";
+import { PasswordRequirements } from "./password-requirements";
+
+export { Button, InputText, InputPassword, FormError, PasswordRequirements };
---
+++ src/features/auth/components/src/input-text.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { cn } from "@/utils/tailwindMerge";
+import { FormError } from "./form-error";
+
+type InputTextProps = {
+  className?: string;
+  error?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(
+  ({ className, error, ...props }, ref) => (
+    <div>
+      <input
+        ref={ref}
+        type="text"
+        className={cn(
+          `w-full px-4 py-3 border-2 border-accent rounded-[120px] focus:outline-none focus:border-primary transition-colors text-text`,
+          className
+        )}
+        {...props}
+      />
+      <FormError error={error} />
+    </div>
+  )
+);
+
+InputText.displayName = "InputText";
+
+export { InputText };
