d5f27d1d85531b6ed3709a92e649d4a56021720c
Jaylo <jaylogalam04@gmail.com>
2025-12-23 15:52:26 +0800

    pages: add auth pages - minor changes to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./index.css";
-
-/* Landing Page */
 import LandingPage from "@pages/LandingPage";
+import LoginPage from "@pages/LoginPage";
+import SignupPage from "@pages/SignupPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </>
   );
